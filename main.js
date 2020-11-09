3a6639b0221aef3062a24e237dad0b3a4fa6f22c
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-08 23:29:33 -0700

    Bones of HTML and CSS. Main.js to come next.

---
+++ main.js
@@ -0,0 +1,5 @@
+// Cached DOM Nodes
+const grid = document.querySelectorAll(".tile");
+const emptyBox = document.querySelectorAll(".display");
+const header = document.querySelector("h2");
+const reset = document.getElementById("reset-button");
